0050fd4eacc51093ea7f4ee3d1cc6c5aeb5a5a0c
Mark Deegan <mark.deegan@tudublin.ie>
2026-02-11 23:38:49 +0000

    Asset Management

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,19 @@ const __dirname = path.dirname(__filename);
 export default {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+ };
